4c4ec8ededc631033cd7f8ab46f89ba6b9ba34e0
Doooeeerte <doooeeerte@spambog.com>
2014-10-17 21:00:13 +0200

    Code formatting

--- crawler.js
+++ crawler.js
@@ -1,23 +1,21 @@
-var scraperjs = require('scraperjs'),
-    router = new scraperjs.Router();
+var scraperjs = require( 'scraperjs' );
+var router = new scraperjs.Router();
 
-router
-    .otherwise(function(url) {
-    console.log("Url '"+url+"' couldn't be routed.");
-});
+router.otherwise( function( url ) {
+  console.log( "Url '"+url+"' couldn't be routed." );
+} );
 
 var path = {};
 
-router.on('https?://(www.)?youtube.com/watch/:id')
-    .createStatic()
-    .scrape(function($) {
-        return $("a").map(function() {
-            return $(this).attr("href");
-        }).get();
-    }, function(links, utils) {
-        console.log(utils.params.id, links);
-        path[utils.params.id] = links
-    })
+router.on( 'https?://(www.)?youtube.com/watch/:id' )
+      .createStatic()
+      .scrape( function( $ ) {
+        return $( 'a' ).map( function() {
+          return $( this ).attr( 'href' );
+        } ).get();
+      }, function( links, utils ) {
+          console.log( utils.params.id, links );
+          path[ utils.params.id ] = links;
+      } );
 
-router.route("https://www.youtube.com/watch/YE7VzlLtp-4", function() {
-});
+router.route("https://www.youtube.com/watch/YE7VzlLtp-4");
