1a99634467d38179cf33d94988f59cb9f6c2ed97
Doooeeerte <doooeeerte@spambog.com>
2014-10-26 12:29:57 +0100

    Gruntfile and package.json update

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function( grunt ) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig( {
+    // Task configuration.
+    jshint: {
+      options: {
+        // Global Variables
+        'globals': {
+        },
+        
+        // Enforcing Options
+        'bitwise':        true,
+        'camelcase':      true,
+        'curly':          true,
+        'eqeqeq':         true,
+        'es3':            true,
+        'freeze':         true,
+        'immed':          true,
+        'indent':         2,
+        'latedef':        true,
+        'maxdepth':       4,
+        'maxstatements':  35,
+        'maxcomplexity':  11,
+        'newcap':         true,
+        'noarg':          true,
+        'noempty':        true,
+        'nonew':          true,
+        'quotmark':       'single',
+        'undef':          true,
+        'unused':         true,
+        'strict':         true,
+        
+        // Environments
+        'browser':        false,
+        'node':           true,
+        'jquery':         true
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'crawler.js'
+      }/*,
+      lib_test: {
+        src: ['lib/** /*.js', 'test/** /*.js']
+      }*/
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: 'crawler.js',
+        tasks: ['jshint:src']
+      }/*,
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }*/
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+};
