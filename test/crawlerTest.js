7a71a5e00b1ab5523af5829bbed150593d6d552a
Doooeeerte <doooeeerte@spambog.com>
2014-10-27 21:16:13 +0100

    Abstract a crawler module and write a test for it

---
+++ test/crawlerTest.js
@@ -0,0 +1,16 @@
+exports.testCrawler = function( test ) {
+  var testResponse;
+
+  var crawlerGenerator = require( '../lib/crawler' );
+  var crawler = crawlerGenerator( 'https://github.com/doooeeerte/offersCrawler', function handleResult( responseObj ) {
+    testResponse = responseObj;
+  } );
+
+  test.ok( crawler !== undefined, 'crawler is non null' );
+
+  crawler();
+
+  test.ok( testResponse.response === 'someStuff', 'crawler test response is "someStuff"' );
+  test.ok( testResponse.debug === 'crawled "https://github.com/doooeeerte/offersCrawler"', 'crawled the given url' );
+  test.done();
+};
