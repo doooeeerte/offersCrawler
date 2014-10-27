3d64a96dbc854dc78bddc8cfa42c25b2aab71e82
Doooeeerte <doooeeerte@spambog.com>
2014-10-27 21:19:03 +0100

    Actually DO something in router test (though not really much for now ;))

--- test/routerTest.js
+++ test/routerTest.js
@@ -2,5 +2,8 @@ exports.testRouter = function( test ) {
   var router = require( '../lib/router' );
 
   test.ok( router !== undefined, 'router is non null' );
+
+  router.route( 'https://www.youtube.com/watch/YE7VzlLtp-4' );
+
   test.done();
 };
