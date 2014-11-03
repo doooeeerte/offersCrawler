405fd6dc0dcd49f3ecb6e455e71bca8f3be6d634
Doooeeerte <doooeeerte@spambog.com>
2014-11-03 23:39:14 +0100

    Refactoring

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
 ( function main() {
   'use strict';
 
-  var bands = require( './bands.json' );
-  var crawl = require( './lib/crawler' );
+  var bands      = require( './bands.json' );
+  var iterate    = require( './lib/crawler' );
   var visualizer = require( './lib/visualizer' );
 
-  var bandsLeft;
   var requestBands = function requestBands( onResult, onFinished ) {
-    bands.forEach( function( entry ) {
-      var band = {
-        maxPrice : 300
-      };
-
-      /* There's two syntaxes for bands:
-         * An array of band name and max price in €
-         * Simple band name, using a max price of 3 €
-      */
-      if ( entry instanceof Array ) {
-        band.name = entry[ 0 ];
-        band.maxPrice = entry[ 1 ] * 100;
-      } else {
-        band.name = entry;
-      }
-
-      crawl( band, function handleResult( result ) {
-        onResult( result, band );
-      }, function() {
-        console.log( 'Done crawling for "' + band.name + '", ' + --bandsLeft + ' bands left' );
-        if ( bandsLeft < 1 ) {
-          onFinished();
-        }
-      } );
-    } );
+    iterate( bands, onResult, onFinished );
   };
 
   var server = require( 'http' ).createServer( function onRequest( request, response ) {
     if ( request.url === '/' ) {
-      bandsLeft = bands.length;
-
       visualizer.startOutput( response );
 
       requestBands( function onResult( el, band ) {
