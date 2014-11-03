cf9d407f2f099473011c8c9e2e9821de03b4793c
Doooeeerte <doooeeerte@spambog.com>
2014-11-03 23:06:28 +0100

    Refactoring, move all visualization functions into an own file

--- main.js
+++ main.js
@@ -2,29 +2,13 @@
   'use strict';
 
   var bands = require( './bands.json' );
-  var bandsLeft;
-
   var crawl = require( './lib/crawler' );
+  var visualizer = require( './lib/visualizer' );
 
-  var startOutput = function startOutput( response ) {
-    response.writeHead( 200, {
-      'content-type': 'text/html; charset=utf-8',
-      'cache-control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
-      pragma: 'no-cache'
-    } );
-    response.write( '<html><head><style type="text/css">' + 
-      'body > div { display: inline-block; max-width: 110px; font-size: 13px; vertical-align: top; margin: 5px; box-shadow: 3px 3px 10px black; }' +
-      'body > div > a { height: 70px; display: inline-block; overflow: hidden; width: 110px; color: black; text-decoration: none; }' +
-      'body > div > div { text-align: right; }' + 
-      '</style><base href="https://www.rebuy.de/kaufen/"/></head><body>' );
-  };
-  var endOutput = function endOutput( response ) {
-    response.end( '</body></html>');
-  };
-
+  var bandsLeft;
   var requestBands = function requestBands( onResult, onFinished ) {
     bands.forEach( function( entry ) {
-      var obj = {
+      var band = {
         maxPrice : 300
       };
 
@@ -33,16 +17,16 @@
          * Simple band name, using a max price of 3 €
       */
       if ( entry instanceof Array ) {
-        obj.name = entry[ 0 ];
-        obj.maxPrice = entry[ 1 ] * 100;
+        band.name = entry[ 0 ];
+        band.maxPrice = entry[ 1 ] * 100;
       } else {
-        obj.name = entry;
+        band.name = entry;
       }
 
-      crawl( obj, function handleResult( result ) {
-        onResult( result, obj );
+      crawl( band, function handleResult( result ) {
+        onResult( result, band );
       }, function() {
-        console.log( 'Done crawling for "' + obj.name + '", ' + --bandsLeft + ' bands left' );
+        console.log( 'Done crawling for "' + band.name + '", ' + --bandsLeft + ' bands left' );
         if ( bandsLeft < 1 ) {
           onFinished();
         }
@@ -50,31 +34,16 @@
     } );
   };
 
-  var server = require( 'http' ).createServer( function( request, response ) {
+  var server = require( 'http' ).createServer( function onRequest( request, response ) {
     if ( request.url === '/' ) {
       bandsLeft = bands.length;
 
-      startOutput( response );
+      visualizer.startOutput( response );
 
       requestBands( function onResult( el, band ) {
-        var maxPrice = band.maxPrice;
-        
-        response.write( '<div><img src="' + el.find( 'img' ).attr( 'data-original' ) + '"/><br/>' );
-        var anchor = el.find( 'h2 a:nth-child(1)' );
-        response.write( '<a target="rebuy" href="' + anchor.attr( 'href' ) + '">' + anchor.text().trim() + '</a><br/>' );
-
-        var price = parseFloat( el.find( '.price strong' ).text().replace( ' €', '' ).replace( ',', '.' ) ) * 100;
-        var col = 'red';
-
-        if ( price * 3 < maxPrice ) {
-          col = 'green';
-        } else if ( price * 1.5 < maxPrice ) {
-          col = 'yellow';
-        }
-
-        response.write( '<div style="background-color: ' + col + '"><b>' + ( price / 100 ) + ' € </b></div></div>' );            
+        visualizer.visualizeBand( response, el, band );
       }, function onFinished() {
-        endOutput( response );
+        visualizer.endOutput( response );
       } );
     } else {
       response.writeHead( 404 );
