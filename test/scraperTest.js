896b40e924eb7495fb0d0fc91602ec2fb8dea03d
Doooeeerte <doooeeerte@spambog.com>
2015-06-04 20:25:18 +0200

    Try reverting to nock 1.7

--- test/scraperTest.js
+++ test/scraperTest.js
@@ -2,12 +2,10 @@ var nockResponses = function nockResponses() {
   'use strict';
 
   var nock = require( 'nock' );
-  //var returnedHTML = require( 'fs' ).readFileSync( './test/sample.html', 'utf8' );
 
   nock( 'https://www.rebuy.de/' )
     .get( '/kaufen/suchen?inStock=1&c=83&q=%22Heaven+Shall+Burn%22&priceMax=2000&sortBy=price_asc' )
     .replyWithFile( 200, __dirname + '/sample.html', 'utf-8' );
-    //.reply( 200, returnedHTML );
 };
 
 exports.testScraper = function testScraper( test ) {
@@ -18,16 +16,12 @@ exports.testScraper = function testScraper( test ) {
 
   nockResponses();
   
-  scrape( 'https://www.rebuy.de/kaufen/suchen?inStock=1&c=83&q=%22Heaven+Shall+Burn%22&priceMax=2000&sortBy=price_asc', 'html', function handleResult( obj ) {
-    console.log( 'Adding object(s)' );
-    console.log( obj );
+  scrape( 'https://www.rebuy.de/kaufen/suchen?inStock=1&c=83&q=%22Heaven+Shall+Burn%22&priceMax=2000&sortBy=price_asc', '.product', function handleResult( obj ) {
     links.push.apply( links, obj );
-    console.log( 'Result is:' );
-    console.log( links );
   } )
   .then( function scrapingDone() {
     console.log( links );
-    test.ok( links.length === 1, 'scraper returned 5 products (actually it was ' + links.length + ')' );
+    test.ok( links.length === 5, 'scraper returned 5 products (actually it was ' + links.length + ')' );
 
     test.done();
   } );
